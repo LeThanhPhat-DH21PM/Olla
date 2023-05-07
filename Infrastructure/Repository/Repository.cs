ae14acc05eee49eec5d7c292c7022ae54f2566cd
Omega <ltphat_21pm@student.agu.edu.vn>
2023-05-07 22:33:21 +0700

    first commit

---
+++ Infrastructure/Repository/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Infrastructure.EF;
+using Infrastructure.Entities;
+using Infrastructure.Entitites;
+using Infrastructure.Generic;
+
+namespace Infrastructure.Repository
+{
+    public interface ISinhVienRepository : IRepository<SinhVien>
+    {
+
+    }
+
+    public class Repository : Repository<SinhVien>, ISinhVienRepository
+    {
+        public Repository(EXDbContext context) : base(context)
+        {
+        }
+    }
+}
